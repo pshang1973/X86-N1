a6341e6b52ae4eecabaa80ca12e4e7471c3070ee
pshang1973 <70361490+pshang1973@users.noreply.github.com>
2020-09-04 15:25:17 +0800

    Initial commit

---
+++ diy-part2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+got clone https://github.com/V2RaySSR/openwrt-packages
+git clone https://github.com/V2RaySSR/small
+git clone https://github.com/tuanqing/install-program package/install-program
